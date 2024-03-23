6bda41b75fdf824982ded176509f8b91a5ee9a17
HarshitChejara <harshitchejara29@gmail.com>
2024-03-23 18:50:09 +0530

    acai_google_analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import { hotjar } from 'react-hotjar';
 import { useEffect } from 'react';
 // import { useRouter } from "next/router";
 // import Head from 'next/head';
+import { Head } from "next/document";
 import Script from "next/script";
 // import Hotjar from '@hotjar/browser';
 // const siteId = 3917682;
@@ -37,7 +38,7 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
 
   return (
     <html lang="en">
-      {/* <Head> */}
+      <Head>
         <Script
          strategy="lazyOnload"
          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
@@ -52,7 +53,7 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
             });
           `}
         </Script>
-      {/* </Head> */}
+      </Head>
       <body className="bg-white {inter.className}">
         <Header />
         {children}
